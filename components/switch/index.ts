75484cc10b461a5272ded431a16141e109faa913
David Alvarez <48391594+aalvag@users.noreply.github.com>
2025-06-11 09:33:58 -0300

    feat(switch): implement switch component with styles, props, and events

--- components/switch/index.ts
+++ components/switch/index.ts
@@ -1,3 +1,19 @@
-import GSwitch from "./Switch.vue";
+import Switch from "./src/switch.vue";
+import {
+  withInstall,
+  withNoopInstall,
+  SFCWithInstall,
+} from "element-plus/es/utils/index.mjs";
 
-export { GSwitch };
+export const GSwitch: SFCWithInstall<typeof Switch> & {
+  Switch: typeof Switch;
+} = withInstall(Switch, {
+  Switch,
+});
+
+export default GSwitch;
+
+export * from './src/switch';
+export * from './src/constants';
+export * from './src/switch.type';
+export type SwitchInstance = InstanceType<typeof Switch>;
