95f48612ce4f28336071f6055127543c95992748
erikfille <109306990+erikfille@users.noreply.github.com>
2025-11-20 17:21:23 -0300

    feat(loader): add new loader message and update style (#154)
    * feat(loader): add new loader message and update style
    
    * feat: Rename loader message update function, add peer dependencies, enhance Storybook documentation, and update loader styling.
    
    ---------
    
    Co-authored-by: Andersson Mesa <andersson.mesa@global66.com>

--- components/loader/Loader.vue
+++ components/loader/Loader.vue
@@ -2,23 +2,31 @@
   <transition name="fade">
     <div
       class="gui-loader"
-      style="background-color: rgba(4, 16, 66, 0.8)"
       v-if="show"
     >
-      <img src="./assets/img/preloader.gif" class="h-12" />
+      <div class="flex flex-col items-center justify-center">
+        <img src="./assets/img/preloader.gif" class="h-20" />
+        <p v-if="message" class="mt-md text-inverse-txt text-4 font-medium max-w-96 mx-auto text-center">
+          {{ message }}
+        </p>
+      </div>
     </div>
   </transition>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'Loader',
+  name: "Loader",
   props: {
     show: {
       default: false,
       type: Boolean,
+    },
+    message: {
+      default: "",
+      type: String,
     },
   },
 });
@@ -26,7 +34,7 @@ export default defineComponent({
 
 <style lang="scss">
 .gui-loader {
-  @apply fixed top-0 left-0 overflow-hidden flex w-full h-full justify-center items-center select-none;
+  @apply fixed top-0 left-0 overflow-hidden flex w-full h-full justify-center items-center select-none bg-grey-800/80;
   z-index: 5000;
 }
 
