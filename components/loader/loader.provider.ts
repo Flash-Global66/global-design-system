5a97774a1a7b78ce9b74066afa0cd5e8744ec778
erikfille <109306990+erikfille@users.noreply.github.com>
2025-11-26 15:37:18 -0300

    Erik/update loader (#157)
    * feat(loader): add new loader message and update style
    
    * feat: Rename loader message update function, add peer dependencies, enhance Storybook documentation, and update loader styling.
    
    * feat(loader): allow multiple messages iteration and shuffle config
    
    * refactor(loader): remove showGLoader message handling
    
    * refactor(loader): extract intervalId to provider to avoid memory leaks and improve state cleaning
    
    * fix(loader): setLoaderMessage function name conflict between provider and composable
    
    ---------
    
    Co-authored-by: Andersson Mesa <andersson.mesa@global66.com>

--- components/loader/loader.provider.ts
+++ components/loader/loader.provider.ts
@@ -3,7 +3,7 @@ import { InjectionKey, Ref, ref } from "vue";
 export type LoaderProviderType = {
   isLoading: Ref<boolean>;
   showGLoader(show: boolean): void;
-  setLoaderMessage(message: string): void;
+  setMessage(message: string): void;
 };
 
 const showLoader: Ref<boolean> = ref(false);
@@ -15,7 +15,7 @@ const initialValues: LoaderProviderType = {
   showGLoader: (show: boolean) => {
     showLoader.value = show;
   },
-  setLoaderMessage: (message: string) => {
+  setMessage: (message: string) => {
     loaderMessage.value = message;
   },
 };
