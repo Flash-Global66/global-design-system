1815db0e1f341c415df24b25a41f316428969058
Andersson Mesa <andersson.mesa@global66.com>
2026-07-12 15:00:21 -0500

    feat(pagination): migrate off element-plus internals to g-utils
    Repoint buildProps, debugWarn, isNumber, useNamespace, CHANGE_EVENT,
    withInstall and SFCWithInstall to @flash-global66/g-utils. Remove dead
    imports (iconPropType, unused PropType/VNode/IconString) and vestigial
    bindings (slots, _globalSize). Normalize package.json: g-utils and
    g-icon-font in dependencies, drop unused g-icon-button peer; element-plus
    stays as peer for the theme-chalk scss @use.

--- components/pagination/index.ts
+++ components/pagination/index.ts
@@ -1,11 +1,11 @@
-import { withInstall, SFCWithInstall } from 'element-plus/es/utils/index.mjs'
+import { withInstall, SFCWithInstall } from '@flash-global66/g-utils';
 
-import Pagination from './src/pagination'
+import Pagination from './src/pagination';
 
 export const GPagination: SFCWithInstall<typeof Pagination> =
-  withInstall(Pagination)
-export default GPagination
+  withInstall(Pagination);
+export default GPagination;
 
-export * from './src/pagination'
-export * from './src/constants'
-export * from './src/composable/usePagination'
+export * from './src/pagination';
+export * from './src/constants';
+export * from './src/composable/usePagination';
